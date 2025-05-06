a68348b360dbb2ec90840b2238171c7c13cb273a
Fabio-Garcia27 <fabio@unipar.br>
2025-05-06 19:20:30 -0300

    Atualizado 06/05/2025

---
+++ .vscode/C/Teste_Algoritmos.c
@@ -0,0 +1,13 @@
+// Fábio 06/05/25 - Código Teste Algoritmos
+/*
+- stdio.h: entrada e saída de dadoscl
+- string.h: manipulação de strings
+- stdlib.h: alocação de memória, conversão de tipos, etc.
+- math.h: funções matemáticas
+- time.h: funções de data e hora
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <math.h> 
+#include <time.h>
