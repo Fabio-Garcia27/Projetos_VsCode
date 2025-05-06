1ba2ef5b284e8de3a907be948a9b3d9292c62996
Fabio-Garcia27 <fabio@unipar.br>
2025-05-06 20:04:59 -0300

    Atulizado 06/05/2025_3

--- .vscode/C/Teste_Algoritmos.c
+++ .vscode/C/Teste_Algoritmos.c
@@ -18,8 +18,8 @@ typedef struct {
     char nome[50], sexo;
     float salario;
 } Funcionario;
-
-int main() {
+// Função sem retorno
+void funcao_f() {
     Funcionario f1;
     f1.id = 1;
     strcpy(f1.nome, "Maria");
@@ -27,8 +27,31 @@ int main() {
 
     printf("ID: %d\n", f1.id);
     printf("Nome: %s\n", f1.nome);
-    printf("Salário: %.2f\n", f1.salario);
+    printf("Salário: %.2f\n", f1.salario);  
+}
+// Função sem retorno
+void funcao_h() {
+    time_t agora = time(NULL);
+    struct tm *data_hora = localtime(&agora);
+
+    printf("Data e Hora: %02d-%02d-%04d %02d:%02d:%02d\n",
+           //data_hora->tm_year + 1900, data_hora->tm_mon + 1, data_hora->tm_mday,
+           data_hora->tm_mday, data_hora->tm_mon + 1, data_hora->tm_year + 1900,  
+           data_hora->tm_hour, data_hora->tm_min, data_hora->tm_sec);
+
+    char buffer[80];
+    strftime(buffer, sizeof(buffer), "%A, %d de %B de %Y %H:%M:%S", data_hora);
+    printf("Data e Hora Formatadas: %s\n", buffer);
+}
+// É o ponto de entrada do programa, onde a execução começa quando o programa é iniciado
+int main() {
+    funcao_f();
+    funcao_h();
 
     return 0;
 }
+
+
+
+
 
