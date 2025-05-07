0d8fe13298532a14744e4f3a5122d169b1b4da82
Fabio-Garcia27 <fabio@unipar.br>
2025-05-07 15:47:49 -0300

    Atualizado 07/05/2025_9

--- .vscode/C/Teste_Algoritmos.c
+++ .vscode/C/Teste_Algoritmos.c
@@ -14,8 +14,8 @@
 #include <conio.h>
 
 // Declaração das variáveis usadas em todo o código
-int A, X, num1, num2, soma;
-float B, item, valor;
+int A, X, num1, num2, num3, soma;
+float B, item, valor, media;
 char C;
 char D[100];
 
@@ -107,6 +107,18 @@ void soma_multi_C() {
     printf("A soma é: %d\n", soma);
     getch(); // Pausa para ver o resultado
 } 
+// Fábio 07-05-25 - Soma e multiplicar
+void calcular_media_C() {
+    printf("Entre com o primeiro número: ");
+    scanf("%i", &num1);
+    printf("Entre com o segundo número: ");
+    scanf("%i", &num2);
+    printf("Entre com o terceiro número: ");
+    scanf("%i", &num3);
+    media = (num1 + num2 + num3) / 3;
+    printf("A média é: %d\n", media);
+    getch(); // Pausa para ver o resultado
+}
 // É o ponto de entrada do programa, onde a execução começa quando o programa é iniciado
 int main() {
     imprimir_funcionario();
@@ -114,6 +126,7 @@ int main() {
     ler_valores();
     biblioteca_C();
     soma_multi_C();
+    calcular_media_C();
 
     return 0;
 }
