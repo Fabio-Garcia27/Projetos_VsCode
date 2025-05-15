620d5444c7942c8957d7d841bde2e3c1bdef9bb0
Fabio-Garcia27 <fabio@unipar.br>
2025-05-15 10:20:56 -0300

    Versão 1.0.0.4

--- .vscode/C/Teste_Guia_Completo.cpp
+++ .vscode/C/Teste_Guia_Completo.cpp
@@ -24,6 +24,7 @@
 #include <ctype.h>
 
 //Declarar variáveis
+int contador = 0;
 
 int func_ope_arit() {
 	int a = 5, b = 3;
@@ -319,6 +320,88 @@ int func_string() {
   return 0;
 }
 
+//Em C, as constantes são valores que não podem ser modificados durante a execução do programa. 
+//Elas são declaradas com a palavra-chave “const” e podem ser do tipo inteiro, flutuante, caractere ou string.
+int func_constantes() {
+	// Declara uma constante inteira chamada "MAX_VALOR" e atribui o valor 10
+    const int MAX_VALOR = 10;
+ 
+  	// Exibe o valor da constante
+  	printf("MAX_VALOR = %d\n", MAX_VALOR);
+ 
+  	// Declara uma variável "a" do tipo inteiro com tamanho reduzido
+  	short a = 32767;
+ 
+  	// Declara uma variável "b" do tipo inteiro com tamanho aumentado
+  	long b = 123456789;
+ 
+  	// Declara uma variável "c" do tipo inteiro com sinal
+  	signed c = -123;
+ 
+  	// Declara uma variável "d" do tipo inteiro sem sinal
+  	unsigned d = 123;
+ 
+ 	 // Declara uma variável "e" do tipo ponto flutuante com precisão simples
+  	float e = 3.14;
+ 
+  	// Declara uma variável "f" do tipo ponto flutuante com precisão dupla
+  	double f = 3.14159265358979323846;
+	/*
+	- %hd para short
+	- %ld para long
+	- %d para signed int
+	- %u para unsigned int
+	- %.2f para float com 2 casas decimais
+	- %.15f para double com 15 casas decimais
+	*/
+	printf("Declaração de Variáveis:\n");
+    printf("a (short): %hd\n", a);
+    printf("b (long): %ld\n", b);
+    printf("c (signed int): %d\n", c);
+    printf("d (unsigned int): %u\n", d);
+    printf("e (float): %.2f\n", e); // %.2f imprime com 2 casas decimais
+    printf("f (double): %.15f\n", f); // %.15f imprime com 15 casas decimais
+
+  getchar();
+  return 0;
+}
+/*
+/Os modificadores de armazenamento são palavras-chave usadas para controlar a maneira como as variáveis são armazenadas em memória em C
+Eles permitem especificar o escopo e a duração de uma variável, além de sua visibilidade em relação a outras partes do código.
+
+“auto”: indica que uma variável é armazenada na memória do stack e tem escopo de bloco (visível apenas dentro do bloco de código onde é declarada). 
+Este é o modificador de armazenamento padrão em C.
+
+“static”: indica que uma variável é armazenada na memória estática e tem escopo de arquivo (visível em todo o arquivo de código onde é declarada). 
+Além disso, uma variável static mantém seu valor entre as chamadas da função onde é declarada.
+
+“extern”: indica que uma variável é armazenada em outro lugar e tem escopo de arquivo (visível em todo o arquivo de código onde é declarada). 
+A variável extern é usada para acessar uma variável global declarada em outro arquivo de código.
+
+egister: é usado para indicar ao compilador que uma variável deve ser armazenada em um registrador, em vez de em uma área de memória, se possível. 
+Isso pode melhorar o desempenho do seu programa, pois acessar um registrador é geralmente mais rápido do que acessar a memória.
+*/
+
+int func_mod_armaz() {
+	int a = 5; // variavel a é automatica e tem escopo de funcao
+	printf("Valor de a: %d\n", a);
+
+	static int b = 5; // variavel a é estatica e tem escopo de funcao
+	printf("Valor de b: %d\n", b);
+	b++;
+
+	extern int contador; // Declara a variável "contador" como externa
+ 	// Incrementa o valor de "contador"
+  	contador++;
+  	printf("contador = %d\n", contador);
+
+	register int c = 5; // a é armazenada em um registrador, se possível
+    printf("Valor de c: %d\n", c);
+
+	getchar();
+	return 0;
+}
+ 
 // executar as funções
 int main(){
 	setlocale(LC_ALL, "Portuguese_Brazil.1252"); // Define o locale para PortuguÉs (Brasil) UTF-8
@@ -336,6 +419,8 @@ int main(){
 	func_vetor();
 	func_struct();
 	func_string();
+	func_constantes();
+	func_mod_armaz();
     // ... seu código aqui ... Descobrir onde está o erro que não abre o programa
     //system("pause"); // ou getchar();
     //return 0;
