8e98140cf1b88700e5deef270c0633ee6540bb5e
Fabio-Garcia27 <fabio@unipar.br>
2025-05-22 08:49:55 -0300

    Versão 1.0.0.1

---
+++ .vscode/C/C_Velocidade_Media.c
@@ -0,0 +1,62 @@
+// Fábio 21/05/25 - Código Velocidade Média
+/*
+- stdio.h: entrada e saída de dadoscl
+- string.h: manipulação de strings
+- stdlib.h: alocação de memória, conversão de tipos, etc.
+- math.h: funções matemáticas
+- time.h: funções de data e hora
+- conio.h: comandos de entrada e saída diversas
+- unistd.h: 
+- sleep(): Faz com que o programa aguarde um determinado período de tempo (em segundos).
+- usleep(): Faz com que o programa aguarde um determinado período de tempo (em microssegundos).
+- fork(): Cria um novo processo.
+- pipe(): Cria um pipe para comunicação entre processos.
+- locale.h: Localidade do programa, definindo como os dados de texto e numéricos devem ser tratados.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <math.h> 
+#include <time.h>
+#include <conio.h>
+#include <unistd.h>
+#include <locale.h>
+
+//Variáveis
+int op=0;
+float vm = 0, s = 0, t = 0;
+    
+int func_vel_media(void){
+    do{
+        system("cls"); // limpar tela em cada passada do laço
+        printf("\n  1 - Velocidade \n 2 - Espaco \n ");
+        scanf("%d", &op);
+        getchar();
+
+        if(op == 1){
+           printf("\n Digite a espaco:" );
+           scanf("%f", &s);
+           getchar();
+           printf("\n Digete o tempo:" );
+           scanf("%f", &t);
+           getchar();
+           printf("\n A velocidade media e: %f", (s/t));
+        } else if (op == 2){
+           printf("\n Digite a velocidade media: "); 
+           scanf("%f", &vm);
+           getchar();
+           printf("\n Digite o tempo: ");
+           scanf("%f", &t);
+           getchar();
+        }
+    }while ((op != 1) && (op != 2));
+    printf("\n Valor digitado errado, só pode ser 1 ou 2.");
+}  
+
+// É o ponto de entrada do programa, onde a execução começa quando o programa é iniciado
+int main() {
+    setlocale(LC_ALL, "Portuguese_Brazil.1252"); // Define o locale para Português (Brasil) UTF-8
+    func_vel_media();
+    getchar();
+    return 0;
+}
