85c0a4904286227312da76d76a16def1bf55cc2e
Fabio-Garcia27 <fabio@unipar.br>
2025-05-26 20:19:35 -0300

    Versão 1.0.0.1

---
+++ .vscode/C/C_nome_quatro_letras.c
@@ -0,0 +1,47 @@
+// Fábio 26/05/2025 - Receber nome e imprimir as 4 primeiras letras
+/*
+- stdio.h: entrada e saída de dadoscl
+- string.h: manipulação de strings
+- stdlib.h: alocação de memória, conversão de tipos, etc.
+- math.h: funções matemáticas
+- time.h: funções de data e hora
+- conio.h: comandos de entrada e saída diversas
+- unistd.h: 
+- sleep(): Faz com que o programa aguarde um determinado período de tempo (em segundos).
+- usleep(): Faz com que o programa aguarde um determinado período de tempo (em microssegundos).
+- fork(): Cria um novo processo.
+- pipe(): Cria um pipe para comunicação entre processos.
+- locale.h: Localidade do programa, definindo como os dados de texto e numéricos devem ser tratados.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h> 
+#include <math.h> 
+#include <time.h>
+#include <conio.h>
+#include <unistd.h>
+#include <locale.h>
+
+// Variáveis
+char nome[25];
+int i;
+
+void func_nome_letras(){
+    printf("\nDigite o nome: ");
+    fgets(nome, 25, stdin);   // E também dessa sintaxe fgets busca o nome corretamente
+    nome[strcspn(nome, "\n")] = 0; // Remover o caractere de nova linha
+    for (i = 0; i < 4; ++i){
+        printf("%c", nome[1]);       
+    }
+    printf("\n");
+    getchar();
+}
+
+// argc será 4 (o nome do programa mais os 3 argumentos)
+// argv será um array contendo: 
+//int main(int argc, char const *argv[]){
+int main(int argc, char const *argv[]){
+    func_nome_letras();
+    getchar();
+    return 0;
+}
